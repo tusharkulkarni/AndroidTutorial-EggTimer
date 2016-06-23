634b81a52e5dbec14f8d7351527772417e050988
tusharkulkarni <tusharkulkarni1990@gmail.com>
2016-06-23 18:42:03 +0530

    Working EggTimer App

--- app/src/main/java/com/tushar/eggtimer/MainActivity.java
+++ app/src/main/java/com/tushar/eggtimer/MainActivity.java
@@ -1,15 +1,27 @@
 package com.tushar.eggtimer;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    SeekBar timerSeekBar;
+    TextView timerText;
+    Boolean isCounterActive = false;
+    Button controllerButton ;
+    CountDownTimer countDownTimer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,47 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        timerText = (TextView)findViewById(R.id.timerText);
+        controllerButton = (Button)findViewById(R.id.controllerButton);
+        Log.i("Info : ", "timerText initialized" + timerText.toString());
+        timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i("Info : ", "updating timer");
+                updateTimer(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void updateTimer(int progress) {
+        int min = progress/60;
+        int sec = progress%60;
+        String secString=Integer.toString(sec);
+        if(sec<10){
+            secString = "0"+secString;
+        }
+
+        String minString=Integer.toString(min);
+        if(min<10){
+            minString = "0"+minString;
+        }
+        Log.i("Info : ", "timerText before updating label : " + timerText.toString());
+        timerText.setText(minString + " : " + secString);
     }
 
     @Override
@@ -48,5 +101,48 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void controlTimer(View view){
+        if(!isCounterActive) {
+
+            isCounterActive = true;
+            timerSeekBar.setEnabled(false);
+            controllerButton.setText("Stop");
+            //add delay because when this method is called, the timerSeekBar.getProgress() is slightly lower
+            // than the actual value we set as some time elapses between the onClick() and this method call
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updateTimer((int) millisUntilFinished / 1000);
+
+                }
+
+                @Override
+                public void onFinish() {
+                    timerText.setText("00:00");
+                    MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+                    mplayer.start();
+                    try{
+                    Thread.sleep(1000);
+                    }catch(Exception e){
+
+                    }
+                    resetTimer();
+                }
+            }.start();
+        }else{
+            resetTimer();
+        }
+    }
+
+    private void resetTimer() {
+        isCounterActive = false;
+        timerSeekBar.setEnabled(true);
+        controllerButton.setText("GO");
+        timerText.setText("00:30");
+        timerSeekBar.setProgress(30);
+        countDownTimer.cancel();
     }
 }
